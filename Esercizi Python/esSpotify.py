b2d277408d6d5569dbdfc57d8d10a9077a8e3ef4
DavideMoldoveanu <72200903+DavideMoldoveanu@users.noreply.github.com>
2020-12-10 16:02:48 +0100

    Update esSpotify.py

--- Esercizi Python/esSpotify.py
+++ Esercizi Python/esSpotify.py
@@ -15,12 +15,15 @@ playlist = []
 dizionario = {}     #dizionario vuoto
 
 for riga in file:
-    rigaTemp = riga[0:-1].split(",")    #dove c'è una virgola lo divide         #mettendo fino a -1 cancella l'ultimo carattere dell'ultima riga perché non ha l'INVIO
-    dizionario["indice"]=rigaTemp[0]    #parte dall'inizio e va fino alla prima virgola
-    dizionario["titolo"]=rigaTemp[1]    #parte dalla prima virgola e va fino alla seconda virgola
-    dizionario["autore"]=rigaTemp[2]    #parte dalla seconda virgola e va fino alla fine (\n non compreso)
-    dizT = dizionario.copy()            #uso dizT perché se no tutti i valori che leggo li perdo e alla fine avrò solo l'ultima canzone
-    playlist.append(dizT)               #le canzoni troppo lunghe non riesce a leggerle..
+    if (riga[-1] == "\n"):                  #faccio questa if così so se sono all'ultima riga del file (perché se sono all'ultima riga non ho un \n e perciò devo prendere la riga dall'inizio alla  fine)
+        rigaTemp = riga[0:-1].split(",")    #dove c'è una virgola lo divide
+    else:
+        rigaTemp = riga.split(",")          #dove c'è una virgola lo divide
+    dizionario["indice"]=rigaTemp[0]        #parte dall'inizio e va fino alla prima virgola
+    dizionario["titolo"]=rigaTemp[1]        #parte dalla prima virgola e va fino alla seconda virgola
+    dizionario["autore"]=rigaTemp[2]        #parte dalla seconda virgola e va fino alla fine (\n non compreso)
+    dizT = dizionario.copy()                #uso dizT perché se no tutti i valori che leggo li perdo e alla fine avrò solo l'ultima canzone
+    playlist.append(dizT)                   #le canzoni troppo lunghe non riesce a leggerle.. 
 
 for element in (playlist):
     print(element["indice"], element["titolo"], element["autore"])
